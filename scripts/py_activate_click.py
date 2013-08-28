3370accdf851b0bb4853cdbbfdd2ff2736d953e2
dhead666 <myfoolishgames@gmail.com>
2013-08-28 22:16:12 +0300

    add scripts

---
+++ scripts/py_activate_click.py
@@ -0,0 +1,9 @@
+# created by dhead666
+import xbmc
+import sys
+
+controlid = sys.argv[1]
+ 
+xbmc.executebuiltin('XBMC.Action(OSD)')
+xbmc.executebuiltin('XBMC.SendClick('+controlid+')')
+
---
+++ scripts/py_select_close.py
@@ -0,0 +1,5 @@
+# created by dhead666
+import xbmc
+ 
+xbmc.executebuiltin('XBMC.Action(Select)')
+xbmc.executebuiltin('XBMC.Action(Close)')
